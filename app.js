3cfec74691828b99534bd2e1cb9197c3c1ab6d9d
Nikolai <niklonberg@gmail.com>
2024-05-19 10:09:42 +0200

    configure jwt check

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 const express = require("express");
 const app = express();
-const jwt = require('express-jwt')
-const jwks = require('jwks-rsa')
+const { expressjwt: jwt } = require("express-jwt");
+const jwks = require("jwks-rsa");
+
+const jwtCheck = jwt({
+  secret: jwks.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: "https://dev-mt7f8veddtr1w2ge.eu.auth0.com/.well-known/jwks.json",
+  }),
+  audience: "http://localhost:5000",
+  issuer: "https://dev-mt7f8veddtr1w2ge.eu.auth0.com/",
+  algorithms: ["RS256"],
+});
 
 app.get("/public", (req, res) => {
   res.json({
@@ -9,7 +21,7 @@ app.get("/public", (req, res) => {
   });
 });
 
-app.get("/private", (req, res) => {
+app.get("/private", jwtCheck, (req, res) => {
   res.json({
     type: "private",
   });
