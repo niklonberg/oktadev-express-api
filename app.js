9cfcecf8f6accba6a55216d52054b8d5998cc625
Nikolai <niklonberg@gmail.com>
2024-05-19 09:41:46 +0200

    install express-jwt, jwks-rsa

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const app = express();
+const jwt = require('express-jwt')
+const jwks = require('jwks-rsa')
 
 app.get("/public", (req, res) => {
   res.json({
     type: "public",
+  });
+});
+
+app.get("/private", (req, res) => {
+  res.json({
+    type: "private",
   });
 });
 
